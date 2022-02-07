93b77d75d9b58112c3bf986e8d6d2a511dd3dac1
Alex Paz <alexmpaz@gmail.com>
2022-02-07 11:51:32 +0000

    Remote JSON decode

--- CupCakeCorner/ContentView.swift
+++ CupCakeCorner/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        // Step 1: create URL we want to read
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")
+        else {
+            print("Invalid URL")
+            return
+        }
+        // Step 2: Fetch data for the URL
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // code to come
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
+        // Step 3: Decode data into Response struct designed above
     }
 }
 
